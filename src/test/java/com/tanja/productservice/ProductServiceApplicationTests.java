a563af2b0a22d834c663521189b0ca7833c5f729
Tanja <tanja.grozdani@student.um.si>
2024-03-07 18:51:23 +0100

    initial commmit

---
+++ src/test/java/com/tanja/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanja.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
